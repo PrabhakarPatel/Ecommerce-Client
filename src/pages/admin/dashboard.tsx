d06a04b9ad1eeff4f9db2f5b563b85f44116aaf2
PrabhakarPatel <prabhakarpatle@gmail.com>
2024-02-01 19:44:48 +0530

    Ecommerce-Client-2024

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -4,13 +4,12 @@ import { FaRegBell } from "react-icons/fa";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { SkeletonLoader } from "../../components/Loader";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import { BarChart, DoughnutChart } from "../../components/admin/Charts";
-import Table from "../../components/admin/DashboardTable";
+import { useStatsQuery } from "../../redux/api/dashboardApi";
 import { RootState } from "../../redux/store";
 import { getLastMonths } from "../../utils/features";
-import { SkeletonLoader } from "../../components/Loader";
-import { useStatsQuery } from "../../redux/api/dashboardApi";
 
 const userImg =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp";
